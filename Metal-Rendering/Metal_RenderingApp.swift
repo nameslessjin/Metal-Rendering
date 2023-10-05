94deec578a890e6fdf8244345ed03868def54a68
nameslessjin <42518630+nameslessjin@users.noreply.github.com>
2023-10-04 19:09:35 -0700

    init

---
+++ Metal-Rendering/Metal_RenderingApp.swift
@@ -0,0 +1,18 @@
+//
+//  Metal_RenderingApp.swift
+//  Metal-Rendering
+//
+//  Created by JINSEN WU on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Metal_RenderingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .navigationTitle("Metal Rendering")
+        }
+    }
+}
